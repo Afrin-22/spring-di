59a3572b7ba2fad19d94a95dfe48f402e932865b
Afrin-22 <58210796+Afrin-22@users.noreply.github.com>
2021-03-27 15:07:22 +0530

    add application.yml, add @ConfigurationProperties to let know springboot config to detect .yml file

--- src/main/java/com/springframework/sdi/config/GreetingServiceConfig.java
+++ src/main/java/com/springframework/sdi/config/GreetingServiceConfig.java
@@ -1,12 +1,12 @@
 package com.springframework.sdi.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 
 import com.springframework.pets.PetService;
 import com.springframework.pets.PetServiceFactory;
@@ -19,6 +19,7 @@ import com.springframework.sdi.services.PrimaryGreetingService;
 import com.springframework.sdi.services.PropertyGreetingService;
 import com.springframework.sdi.services.SetterGreetingService;
 
+@ConfigurationProperties
 @ImportResource("classpath:sdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
